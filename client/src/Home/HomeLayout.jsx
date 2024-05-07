a369615679b8baa4a74399dc234e271c9e8659d2
Abhishek Pandey <abhishekpandey095155@gmail.com>
2024-05-07 22:55:06 +0530

    made some changes

--- client/src/Home/HomeLayout.jsx
+++ client/src/Home/HomeLayout.jsx
@@ -1,11 +1,31 @@
+import  {useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
+import Loader from "../components/Loader";
 import { Outlet } from "react-router-dom";
+
 const HomeLayout = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Set loading to false after 6 seconds delay
+    }, 3000);
+
+    return () => clearTimeout(timer); // Clear timeout on unmount or rerender
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Outlet />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navbar />
+          <Outlet />
+        </>
+      )}
     </>
   );
 };
+
 export default HomeLayout;
---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="loader-container">
+      <img src="./Images/loaderImg.gif" alt="Loading" className="loader-img" />
+    </div>
+  );
+}
+
+export default Loader;
